c125a37aee4275fb02831bdc769346907d1a8543
piyush12346789 <piyushgupta024@gmail.com>
2020-10-21 20:17:01 +0530

    [Piyush] Add: Method to find the maximum out of the 3 given integers.

---
+++ TestMaximum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TestMaximum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int TC1 = MaximumNumberCheck.MaximumIntegerNumber(3,2,1);
+            int TC2 = MaximumNumberCheck.MaximumIntegerNumber(20,30,10);
+            int TC3 = MaximumNumberCheck.MaximumIntegerNumber(200,100,300);
+            Console.WriteLine("Maximum number in TC1 is: {0}",TC1);
+            Console.WriteLine("Maximum number in TC2 is: {0}",TC2);
+            Console.WriteLine("Maximum number in TC3 is: {0}",TC3);
+        }
+    }
+}
